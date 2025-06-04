37e2919386acb7a3016b90bc361ef4ee62ee6124
bnutss <b.nutss@mail.com>
2025-06-04 16:55:46 +0500

    new commit

--- ColorService/settings.py
+++ ColorService/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/services/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
